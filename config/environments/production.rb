6013adc384db67162d0319654dff33f3df6f58cc
gabrielghost <charlie.gavins@neonseed.com>
2017-03-03 22:06:23 +0000

    amending origins to hopefully work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-#   config.action_cable.mount_path = '/cable'
-#   config.action_cable.url = 'wss://bitip-api.herokuapp.com
-# '
-#   config.action_cable.allowed_request_origins = [ '*' ]
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'wss://bitip-api.herokuapp.com
+'
+config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
